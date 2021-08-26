8a3407d79eb13962ab4434bb11bd3db1e98b2eee
elight <eric@homathkotech.com>
2021-08-26 13:12:24 -0700

    Initial commit

---
+++ FlightFu/FlightFuApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlightFuApp.swift
+//  FlightFu
+//
+//  Created by Eric Lightfoot on 2021-08-26.
+//
+//
+
+import SwiftUI
+
+@main
+struct FlightFuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
